e07a2345263fc6a4dbb0648ccdaefe66f4bb120d
NashibJr <nashibjr@gmail.com>
2025-03-04 10:28:01 +0300

    Feat: I have added the registration functionality

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,10 @@ const pathname = window.location.pathname;
 const showBtn = document.getElementById("hideShow");
 const inputs = document.querySelectorAll(".input-content");
 
+const users = JSON.parse(localStorage.getItem("users"));
+
+console.log(users, "::::");
+
 const handlePasswordViewAndHide = () => {
   const text = showBtn.innerHTML;
   switch (text) {
@@ -20,5 +24,30 @@ const handlePasswordViewAndHide = () => {
 if (pathname === "/src/signup/signup.html") {
   showBtn.addEventListener("click", () => {
     handlePasswordViewAndHide();
+  });
+  document.querySelector("form").addEventListener("submit", (event) => {
+    event.preventDefault();
+    const inputs = document.querySelectorAll("input");
+    let person = {};
+    person = Object.assign(person, {
+      username: inputs[0].value,
+      password: inputs[1].value,
+    });
+    const exists = users?.find((user) => user.username === person.username);
+    if (exists) {
+      alert(`${exists.username} already exists`);
+      return;
+    }
+    users.push(person);
+    localStorage.setItem("users", JSON.stringify(users));
+    if (person.username !== "") {
+      alert(`${person.username} is successfully registered`);
+      person = {};
+      for (let input of inputs) {
+        input.value = "";
+      }
+    } else {
+      console.log("An unexpected error has occured");
+    }
   });
 }
